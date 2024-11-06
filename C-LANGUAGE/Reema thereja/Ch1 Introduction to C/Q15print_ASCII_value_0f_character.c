e56ab32f452387e6ad4e59a2f7a3ef632f51307c
Jeetu-mehra <jeetumehramtr@gmail.com>
2024-11-06 22:50:03 +0530

    OLD

--- C-LANGUAGE/Reema thereja/Ch1 Introduction to C/Q15print_ASCII_value_0f_character.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    char a;
-    printf("Enter any character: ");
-    scanf("%c",&a);
-    printf("The ASCII value of '%c' is = %d",a,a);
-}
--- C-LANGUAGE/Reema thereja/Ch1 Introduction to C/Q20Convert_fahrenheit_into_cel.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    float cel,fah;
-    printf("Enter temprature in fahrenheit: ");
-    scanf("%f",&fah);
-    cel=(0.56)*(fah-32);
-    printf("The temprature in degree celcius is: %f",cel);
-}
